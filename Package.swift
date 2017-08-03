785ff916637e38ed50f657add4851ae6c42847e9
Andrey Kladov <andreyakladov@gmail.com>
2017-08-03 13:47:18 +0200

    upd: Package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporAbortPatterns", 
+    name: "AbortPatterns", 
     dependencies:[
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
